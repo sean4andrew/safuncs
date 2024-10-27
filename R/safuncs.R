2c76ba6d788d8b4f140f400e8ff24b5bd2abbbd0
Sean A. <sean4andrew@gmail.com>
2024-10-27 17:16:49 -0300

    various documentation stuff

--- R/safuncs.R
+++ R/safuncs.R
@@ -877,7 +877,7 @@ Surv_Gen = function(mort_db,
 #' @param xlab A string specifying the plot x-axis label.
 #' @param lambda Smoothing value for the hazard curve. Higher lambda produces greater smoothing. Defaults to NULL where \code{bshazard()} uses the provided survival data to estimate lambda; NULL specification is recommended for large sample size situations which usually occurs on our full-scale studies with many mortalities and tank-replication. At low sample sizes, the lambda estimate can be unreliable. Choosing a lambda of 10 (or anywhere between 1-100) probably produces the most accurate hazard curve for these situations. In place of choosing lambda, choosing \code{phi} is recommended; see below.
 #' @param phi Dispersion parameter for the count model used in hazard curve estimation. Defaults to NULL where \code{bshazard()} uses the provided survival data to estimate phi; NULL specification is recommended for large sample size situations. At low sample sizes, the phi estimate can be unreliable. Choosing a phi value of 1 for low sample sizes is recommended. This value of 1 (or close) seems to be that estimated in past Tenaci data (QCATC997; phi ~ 0.8-1.4) where there are large sample sizes with tank-replication. The phi value of 1 indicates the set of counts (deaths) over time have a Poisson distribution, following the different hazard rates along the curve and are not overdispersed (phi > 1).
-#' @param dailybin Whether to set time bins at daily (1 TTE) intervals. Refer to the \code{bshazard()} documentation for an understanding on the role of bins to hazard curve estimation. Please set to TRUE at low sample sizes and set to FALSE at large sample sizes with tank-replication. Defaults to TRUE.
+#' @param dailybin Whether to set time bins at daily (1 TTE) intervals. Refer to the \code{bshazard()} documentation for an understanding on the role of bins to hazard curve estimation. Please set to TRUE at low sample sizes and set to FALSE at large sample sizes with tank-replication. Defaults to FALSE.
 #' @param plot Which plot to output. Use "surv" for the Kaplan-Meier Survival Curve, "haz" for the Hazard Curve, or "both" for both. Defaults to "both".
 #' @param colours Vector of color codes for the different treatment groups in the plot. Defaults to ggplot2 default palette.
 #' @param theme Character string specifying the graphics theme for the plots. Theme "ggplot2" and "prism" currently available. Defaults to "ggplot2".
@@ -916,7 +916,7 @@ Surv_Plots = function(surv_db,
                       xlab = "Days Post Challenge",
                       lambda = NULL,
                       phi = NULL,
-                      dailybin = TRUE,
+                      dailybin = FALSE,
                       plot = "both",
                       colours = NULL,
                       theme = "ggplot",
---
+++ README.md
@@ -0,0 +1,13 @@
+
+# safuncs
+
+<!-- badges: start -->
+<!-- badges: end -->
+
+Functions to simulate, predict, generate, and plot biological data. 
+
+[Get started](https://sean4andrew.github.io/safuncs/articles/safuncs.html) showcases some core functions in the package that makes analysis quick and easy.
+
+[Reference](https://sean4andrew.github.io/safuncs/reference/index.html) lists all functions and example datasets with documentation.
+
+
